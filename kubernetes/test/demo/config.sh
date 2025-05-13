7322ecd42bd13d43be2a814862f3d8e8ff226138
Janos Czentye <czentye@tmit.bme.hu>
2025-05-13 15:47:15 +0200

    Make all config variables also exported for envsubst Add config vars

--- kubernetes/test/demo/config.sh
+++ kubernetes/test/demo/config.sh
@@ -12,20 +12,32 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+set -o allexport    # Also export all variables for envsubst
 
+# Logging
 SCRIPTS_DIR=$(readlink -f "$(dirname "$0")")
 source "${SCRIPTS_DIR}/../scripts/helper.sh"
-
-TIMEOUT=60s
+# Cluster
+TIMEOUT=60
 CLUSTER=demo
 NODE_A=node-a
 NODE_B=node-b
 NODE_AB=node-ab
-
+# Privacy zones
 PZ_A=zone-A
 PZ_B=zone-B
-
+# PTX-edge components
 API_IMG=ptx-edge/rest-api:1.0
 BUILDER_IMG=ptx-edge/builder:1.0
 PDC_IMG=ptx/connector:1.9.2-slim
 MONGODB_IMG=ptx/mongodb:8.0.5-slim
+# REST-API
+PTX=ptx-edge
+REST_API=rest-api
+API_PORT=8080
+PREFIX=ptx-edge/v1
+# PDC
+PDC=pdc
+PDC_PORT=3000
+PDC_NODE_PORT=30003
+PDC_PREFIX=ptx-edge/pdc
