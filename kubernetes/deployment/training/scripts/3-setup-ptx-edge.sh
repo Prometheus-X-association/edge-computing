5f5925427677cb525bfb97ef366295c5e15a31b3
Janos Czentye <czentye@tmit.bme.hu>
2026-05-12 17:48:00 +0200

    Add retry settings to API checkers in training setup

--- kubernetes/deployment/training/scripts/3-setup-ptx-edge.sh
+++ kubernetes/deployment/training/scripts/3-setup-ptx-edge.sh
@@ -69,7 +69,8 @@ for pz in ${PZ_DATA_0} ${PZ_DATA_1}; do
     ${KCTL} wait --for=jsonpath='{.status.loadBalancer.ingress[].ip}' --timeout="${TIMEOUT}s" "ingress/${PDC}-${pz}"
     _PDC_URL="https://${CLUSTER_HOST}/${PTX}/${pz}/${PDC}"
     log ">>> PDC is exposed on ${_PDC_URL}"
-    wget --spider -S -nv --no-check-certificate "${_PDC_URL}"
+    wget --spider -S -nv --no-check-certificate --retry-on-http-error=500,502 --waitretry=2 --read-timeout=3 --tries=10 \
+            "${_PDC_URL}"
     echo
     curl -k -LSsf "${_PDC_URL}" | grep "href" | head -n1
     echo
@@ -88,11 +89,11 @@ ${KCTL} wait --for=jsonpath='{.status.loadBalancer.ingress[].ip}' --timeout="${T
 
 _REST_API_URL="https://${CLUSTER_HOST}/${PREFIX}/ui/"
 log ">>> ${REST_API} is available on ${_REST_API_URL}"
-wget --spider -S -nv --no-check-certificate --tries=5 --read-timeout=5 \
+wget --spider -S -nv --no-check-certificate --retry-on-http-error=500,502 --waitretry=2 --read-timeout=3 --tries=10 \
     --user="${API_BASIC_USER}" --password="${API_BASIC_PASSWORD}" "${_REST_API_URL}"
 curl -k -u "${API_BASIC_USER}:${API_BASIC_PASSWORD}" "https://${CLUSTER_HOST}/${PREFIX}/versions" | python3 -m json.tool
-log ">>> ${REST_API} is also exposed on https://${PRIMARY_HOST}/${PREFIX}/ui/"
-log ">>> ${REST_API} is also exposed on https://${GW_HOST}/${PREFIX}/ui/"
+log ">>> ${REST_API} is also exposed on https://${PRIMARY_HOST}/${PREFIX}/ui/\n
+>>> ${REST_API} is also exposed on https://${GW_HOST}/${PREFIX}/ui/"
 
 ########################################################################################################################
 
--- kubernetes/deployment/training/scripts/4-run-demo.sh
+++ kubernetes/deployment/training/scripts/4-run-demo.sh
@@ -59,11 +59,12 @@ ${KCTL} get all,ingress -l "app.kubernetes.io/name=${AGG}"
 
 log "Waiting for ingress to set up[10s]..." && sleep 10
 ${KCTL} wait --for=jsonpath='{.status.loadBalancer.ingress[].ip}' --timeout="${TIMEOUT}s" "ingress/${AGG}-mlflow-ui"
-_AGG_URL="https://${CLUSTER_HOST}/worker/${AGG}/"
+_AGG_URL="https://${CLUSTER_HOST}/worker/${AGG}"
 log ">>> Aggregator is available on ${_AGG_URL}"
-wget --spider -S -nv --no-check-certificate --user="${API_BASIC_USER}" --password="${API_BASIC_PASSWORD}" "${_AGG_URL}"
-log ">>> Aggregator is also exposed on https://${PRIMARY_HOST}/worker/${AGG}/"
-log ">>> Aggregator is also exposed on https://${GW_HOST}/worker/${AGG}/"
+wget --spider -S -nv --no-check-certificate --user="${API_BASIC_USER}" --password="${API_BASIC_PASSWORD}" \
+        --retry-on-http-error=500,502 --waitretry=2 --read-timeout=3 --tries=10 "${_AGG_URL}"
+log ">>> Aggregator is also exposed on https://${PRIMARY_HOST}/worker/${AGG}\n
+>>> Aggregator is also exposed on https://${GW_HOST}/worker/${AGG}/"
 
 ########################################################################################################################
 
@@ -82,9 +83,10 @@ log "Waiting for ingress to set up[10s]..." && sleep 10
 ${KCTL} wait --for=jsonpath='{.status.loadBalancer.ingress[].ip}' --timeout="${TIMEOUT}s" "ingress/${ORCH}"
 _ORCH_URL="https://${CLUSTER_HOST}/worker/${ORCH}/docs"
 log ">>> Orchestrator is available on ${_ORCH_URL}"
-wget --spider -S -nv --no-check-certificate --user="${API_BASIC_USER}" --password="${API_BASIC_PASSWORD}" "${_ORCH_URL}"
-log ">>> Orchestrator is also exposed on https://${PRIMARY_HOST}/worker/${ORCH}/docs"
-log ">>> Orchestrator is also exposed on https://${GW_HOST}/worker/${ORCH}/docs"
+wget --spider -S -nv --no-check-certificate --user="${API_BASIC_USER}" --password="${API_BASIC_PASSWORD}" \
+        --retry-on-http-error=500,502 --waitretry=2 --read-timeout=3 --tries=10 "${_ORCH_URL}"
+log ">>> Orchestrator is also exposed on https://${PRIMARY_HOST}/worker/${ORCH}/docs\n
+>>> Orchestrator is also exposed on https://${GW_HOST}/worker/${ORCH}/docs"
 
 ########################################################################################################################
 echo -e "\nDone."
